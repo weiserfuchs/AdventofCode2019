8e86dea19cc9575ae8147e4c45e77d3377b885ef
weiserfuchs <weiserfuchs@gmx.net>
2021-10-21 00:20:53 +0200

    asd

--- aoc_04.py
+++ aoc_04.py
@@ -2,7 +2,7 @@ bDouble = False
 bGreater = True
 iPre = 0
 iCounter = 0
-
+iDoubles = 1
 
 for i in range(265275,781584):
     tmp = str(i)
@@ -12,11 +12,23 @@ for i in range(265275,781584):
             bGreater = False
         if digit == iPre:
             bDouble = True
+            iDoubles +=1
+            #print("Double Counter ",iDoubles," i:",i )
+        else:
+            if iDoubles == 2:
+                bDouble = True
+                iDoubles = 1
+            else:
+                bDouble = False
+                iDoubles = 1
         iPre = digit
-    if ((bDouble == True) and (bGreater == True)):
+    if ((bDouble == True) and (bGreater == True) and (iDoubles % 2 == 0)):
+        #print(i)
         iCounter += 1
     iPre = 0
     bGreater = True
     bDouble = False
+    iDoubles = 1
+
 
 print(iCounter)
